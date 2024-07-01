5d249dc7de3015a27ca9c8e517546724ce401adc
Hasitha Dananjaya <140165356+Hasitha12389@users.noreply.github.com>
2024-07-01 20:31:23 +0530

    q2.scala

--- q2.scala
+++ q2.scala
@@ -6,8 +6,11 @@ def stringFilter(arr: List[String]): List[String] = {
 }
 
     def main(args: Array[String]): Unit={
-        val arr1 = List("Namal","Aberathna","Thomson","Egon","Ramsis","Mewan","Sunil")
-        val arr2 = stringFilter(arr1)
+       // val arr1 = List("Namal","Aberathna","Thomson","Egon","Ramsis","Mewan","Sunil")
+        print("Enter the list of strings using spaces: ")
+        val arr1 = scala.io.StdIn.readLine()   // Read the line and return it as a string
+        val arr3 = arr1.split(" ").toList      // break that string using spaces and store them as a list
+        val arr2 = stringFilter(arr3) 
         print(arr2)
 }
 }
