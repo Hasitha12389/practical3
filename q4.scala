5e01040df819d3ec4e6ace6a5839809c5d43f9e6
Hasitha Dananjaya <140165356+Hasitha12389@users.noreply.github.com>
2024-07-01 20:32:58 +0530

    q4.scala

--- q4.scala
+++ q4.scala
@@ -4,8 +4,11 @@ object q4{
     }
 
     def main(args: Array[String]): Unit={
-        val temp = List(1,2,3,4,5,6,7,8,9,10)
-        val temp2 = evenSum(temp)
+       // val temp = List(1,2,3,4,5,6,7,8,9,10)
+       print("Enter the numbers you like using spaces : ")
+        val temp = scala.io.StdIn.readLine()     // Read the Input as String 
+        var temp3 = temp.split(" ").map(_.toInt).toList    // split that string using spaces and convert that strings to integers and store them in list
+        val temp2 = evenSum(temp3)
         print(temp2)
 
     }
