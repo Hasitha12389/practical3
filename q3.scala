1caf4da3f53c6abc239512b5b41bbed3f44e14d4
Hasitha Dananjaya <140165356+Hasitha12389@users.noreply.github.com>
2024-07-02 10:52:08 +0530

    q3.scala

--- q3.scala
+++ q3.scala
@@ -3,10 +3,10 @@ object q3{
         num1 + num2
        }
      
-    def average(num1: Int,num2: Int): Double={
+    def average(num1: Int,num2: Int): BigDecimal={
         val num3 = add(num1,num2)
         val num4 = (num3/2)
-        BigDecimal(num4).setScale(2).toDouble                 // num4 rounded to two decimal places
+        BigDecimal(num4).setScale(2)                // num4 rounded to two decimal places
     }
 
     def main(args: Array[String]): Unit={
